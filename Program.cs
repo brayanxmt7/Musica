261ccae2f6a7967a68c2da8b768c91574d3572c2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-04 20:28:23 -0300

    EU amo músicas

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(@"
+os filhos ja estao chegando aqui agora somos dois ou tres ou mais 
+encontro meu coracao disposto a queimar por ti
+");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(@"
+todos os versos e cancoes que eu consegui cantar todas as vezes quebrantado so quero te falar
+teu e o reino e a gloria pra sempre
+");
+
+        Console.ResetColor();
+        Console.WriteLine(@"
+teu e o dominio e o poder amem 
+amemmm
+");
+    }
+}
